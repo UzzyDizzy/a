ddd3f096ca51b90e0d6aa03345969fb636a62488
UzzyDizzy <uditsenapaty@gmail.com>
2025-01-10 04:30:18 +0530

    Initial commit 1

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const PORT = 4000;
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there yo' });
+app.get('/home', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
 });
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
+
+module.exports = app;
